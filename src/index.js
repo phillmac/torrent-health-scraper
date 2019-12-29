b4e69c3cd559b0e7ff649a89ac551266890ebd0c
phillmac <phillmac@users.noreply.github.com>
2019-12-29 17:11:39 +1100

    Only recycle after run is complete

--- src/index.js
+++ src/index.js
@@ -61,6 +61,10 @@ async function run () {
   } else {
     console.debug('Already running')
   }
+
+  if (doRecycle) {
+    process.exit()
+  }
 }
 
 async function scrape (torrent) {
@@ -178,8 +182,13 @@ function isStaleDHT (torrent) {
 
 const runInterval = process.env.RUN_INTERVAL ? parseInt(process.env.RUN_INTERVAL) : 30
 
+var doRecycle = false
+
+
 setInterval(run, runInterval * 1000)
 
 if (process.env.RECYCLE_TIMEOUT) {
-  setTimeout(process.exit, parseInt(process.env.RECYCLE_TIMEOUT) * 1000)
+  setTimeout(function () {
+    doRecycle = true
+  }, parseInt(process.env.RECYCLE_TIMEOUT) * 1000)
 }
