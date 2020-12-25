ae60abc382895108f48c87f75b0e64e00a5bc67e
phillmac <phillmackintosh@gmail.com>
2020-12-25 23:32:52 +0000

    Remove debug msg

--- src/debug.js
+++ src/debug.js
@@ -105,7 +105,6 @@ if (process.env.TORRENT_HASH !== '' && process.env.TORRENT_HASH !== undefined) {
   (async () => {
     const fs = require('fs')
     const hashesRaw = fs.readFileSync(0, 'utf-8')
-    console.log(hashesRaw, 'hashesRaw')
     const hashes = hashesRaw.split(' ')
     for (const h of hashes) {
       console.info(`Debugging hash ${h}`)
