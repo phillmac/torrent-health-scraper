b04db0375d9deefe405a287d7c3f87de88eae8cb
phillmac <phillmackintosh@gmail.com>
2020-12-25 01:05:59 +0000

    Add add.js

---
+++ src/add.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const { docopt } = require('docopt')
+const { version } = require('../package.json')
+
+class Cli {
+  constructor () {
+    const doc =
+      `
+Add v${version}
+Usage:
+    add.js --torrent-url=TORRENT_URL [options]
+    add.js -h | --help | --version
+Options:
+    --redis-host=REDIS_HOST             Connect to redis on REDIS_HOST
+    --redis-port=REDIS_PORT             Connect to redis on REDIS_PORT
+    --max-age=MAX_AGE                   Maximum age before considering torrent health info stale
+`
+    const _args = docopt(doc, {
+      version: version
+    })
+    this.args = () => {
+      return _args
+    }
+  }
+}
+
+const cli = new Cli()
+const args = cli.args()
+
+if (args['--redis-host']) {
+  process.env.REDIS_HOST = args['--redis-host']
+}
+
+if (args['--redis-port']) {
+  process.env.REDIS_PORT = args['--redis-port']
+}
+
+if (args['--torrent-url']) {
+  process.env.TORRENT_URL = args['--torrent-url']
+}
+
+async function run () {
+  if (process.env.TORRENT_URL) {
+    add(await torrentFromUrl(process.env.TORRENT_URL))
+  }
+}
+
+async function add (torrent) {
+    const { redisClient } = require('./redis.js')
+    const { infoHash } = torrent
+    const exists = null !== await redisClient.hgetAsync('torrents', infoHash)
+    console.log({exists, torrent})
+}
+
+run()
