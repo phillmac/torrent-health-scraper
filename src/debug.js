5bd5be853003777dc4d6a07d44a78d0d3dae8ab5
phillmac <phillmackintosh@gmail.com>
2020-12-25 22:48:13 +0000

    Add docopt for cli interface

--- src/debug.js
+++ src/debug.js
@@ -1,5 +1,44 @@
 'use strict'
 
+const { docopt } = require('docopt')
+const { version } = require('../package.json')
+
+class Cli {
+  constructor () {
+    const doc =
+      `
+Debug v${version}
+Usage:
+    debug.js [--torrent-hash=TORRENT_HASH] [options]
+    debug.js -h | --help | --version
+Options:
+    --redis-host=REDIS_HOST             Connect to redis on REDIS_HOST
+    --redis-port=REDIS_PORT             Connect to redis on REDIS_PORT
+`
+    const _args = docopt(doc, {
+      version: version
+    })
+    this.args = () => {
+      return _args
+    }
+  }
+}
+
+const cli = new Cli()
+const args = cli.args()
+
+if (args['--redis-host']) {
+  process.env.REDIS_HOST = args['--redis-host']
+}
+
+if (args['--redis-port']) {
+  process.env.REDIS_PORT = args['--redis-port']
+}
+
+if (args['--torrent-hash']) {
+  process.env.TORRENT_HASH = args['--torrent-hash']
+}
+
 async function debugScrape (hash) {
   const { redisClient, lock } = require('./redis.js')
   const functions = (require('./functions.js')(redisClient, lock, true))
