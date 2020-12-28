1dde89848b8f1541255554962ac4739c6ee499c3
phillmac <phillmackintosh@gmail.com>
2020-12-28 06:53:24 +0000

    Fetch torrent later

--- src/debug.js
+++ src/debug.js
@@ -54,13 +54,13 @@ async function debugScrape (hash) {
       const trackerIgnore = await redisClient.smembersAsync('tracker_ignore')
       console.debug('Waiting for queue lock')
       unlock = await lock('qLock')
-      const torrent = JSON.parse(await redisClient.hgetAsync('torrents', hash))
       console.debug('Fetching queue contents')
       const queued = await redisClient.smembersAsync('queue')
       if (hash in queued) {
-        console.error(`Hash ${hash} is already queued`)
-        unlock()
-      } else {
+          console.error(`Hash ${hash} is already queued`)
+          unlock()
+        } else {
+        const torrent = JSON.parse(await redisClient.hgetAsync('torrents', hash))
         const isStale = functions.isStale(torrent, trackerIgnore)
         const isStaleDHT = functions.isStaleDHT(torrent)
         const dhtScraped = torrent?.dhtData?.scraped_date
