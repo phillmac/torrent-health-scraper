21154cbe75783837e68e4c33b97ac0f2933d43e3
phillmac <phillmackintosh@gmail.com>
2021-02-09 00:03:11 +0000

    Fix syntax

--- src/index.js
+++ src/index.js
@@ -5,37 +5,39 @@ const functions = (require('./functions.js')(redisClient, lock))
 
 let lockout = false
 
-async function getWorkItems () {
-  const torrentKeys = await redisClient.hkeysAysnc('torrents')
-  const trackerIgnore = await redisClient.smembersAsync('tracker_ignore')
-  const candidates = torrents
-    .filter(t => functions.isStale(t, trackerIgnore))
-  const unlock = await lock('qLock')
-  const queued = await redisClient.smembersAsync('queue')
-  const staleTorrents = candidates
-    .filter(t => !(queued.includes(t._id)))
-  if (staleTorrents.length > 0) {
-    await redisClient.saddAsync('queue', staleTorrents.map(t => t._id))
-  }
-  unlock()
-  return staleTorrents
-}
-
-async function process (workItems) {
-  for (const wItem of workItems) {
-    try {
-      await redisClient.hsetAsync('torrents', wItem._id, JSON.stringify(await functions.scrape(wItem, trackerIgnore)))
-      await redisClient.sremAsync('queue', wItem._id)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-}
-
 async function run () {
   if (!lockout) {
     try {
       lockout = true
+      const trackerIgnore = await redisClient.smembersAsync('tracker_ignore')
+
+      async function getWorkItems () {
+        const torrents = await redisClient.hgetallAysnc('torrents')
+        const trackerIgnore = await redisClient.smembersAsync('tracker_ignore')
+        const candidates = torrents
+          .filter(t => functions.isStale(t, trackerIgnore))
+        const unlock = await lock('qLock')
+        const queued = await redisClient.smembersAsync('queue')
+        const staleTorrents = candidates
+          .filter(t => !(queued.includes(t._id)))
+        if (staleTorrents.length > 0) {
+          await redisClient.saddAsync('queue', staleTorrents.map(t => t._id))
+        }
+        unlock()
+        return staleTorrents
+      }
+
+      async function process (workItems) {
+        for (const wItem of workItems) {
+          try {
+            await redisClient.hsetAsync('torrents', wItem._id, JSON.stringify(await functions.scrape(wItem, trackerIgnore)))
+            await redisClient.sremAsync('queue', wItem._id)
+          } catch (err) {
+            console.error(err)
+          }
+        }
+      }
+
       const workItems = getWorkItems()
       if (workItems.length > 0) {
         await process(workItems)
