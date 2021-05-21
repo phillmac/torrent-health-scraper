b03e8e0b8f38ae6303304b0c744db65db492501b
phillmac <phillmackintosh@gmail.com>
2021-05-21 08:35:10 +0000

    Add docopt cli options

--- src/populate.js
+++ src/populate.js
@@ -1,3 +1,45 @@
+'use strict'
+
+const { docopt } = require('docopt')
+const { version } = require('../package.json')
+
+class Cli {
+    constructor () {
+        const doc =
+        `
+Preload v${version}
+Usage:
+    preload.js [--torrents-file=TORRENTS_FILE] [options]
+    preload.js -h | --help | --version
+Options:
+    --redis-host=REDIS_HOST             Connect to redis on REDIS_HOST
+    --redis-port=REDIS_PORT             Connect to redis on REDIS_PORT
+        `
+        const _args = docopt(doc, {
+            version: version
+        })
+        this.args = () => {
+            return _args
+        }
+    }
+}
+
+const cli = new Cli()
+const args = cli.args()
+
+if (args['--redis-host']) {
+    process.env.REDIS_HOST = args['--redis-host']
+}
+
+if (args['--redis-port']) {
+    process.env.REDIS_PORT = args['--redis-port']
+}
+
+if (args['--torrents-file']) {
+    process.env.TORRENTS_FILE = args['--torrents-file']
+}
+
+
 if (!process.env.TORRENTS_FILE) {
   throw Error('TORRENTS_FILE is required')
 }
