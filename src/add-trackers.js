326b4f0c3a2c27a39077083020e7d47caf82b2ed
phillmac <phillmackintosh@gmail.com>
2021-05-21 05:59:53 +0000

    Add docopt cli options

--- src/add-trackers.js
+++ src/add-trackers.js
@@ -1,3 +1,44 @@
+'use strict'
+
+const { docopt } = require('docopt')
+const { version } = require('../package.json')
+
+class Cli {
+  constructor () {
+    const doc =
+      `
+Add-Trackers v${version}
+Usage:
+    add-trackers.js [--trackers-file=TRACKERS_FILE] [options]
+    add-trackers.js -h | --help | --version
+Options:
+    --redis-host=REDIS_HOST             Connect to redis on REDIS_HOST
+    --redis-port=REDIS_PORT             Connect to redis on REDIS_PORT
+`
+    const _args = docopt(doc, {
+      version: version
+    })
+    this.args = () => {
+      return _args
+    }
+  }
+}
+
+const cli = new Cli()
+const args = cli.args()
+
+if (args['--redis-host']) {
+  process.env.REDIS_HOST = args['--redis-host']
+}
+
+if (args['--redis-port']) {
+  process.env.REDIS_PORT = args['--redis-port']
+}
+
+if (args['--trackers-file']) {
+  process.env.TRACKERS_FILE = args['--trackers-file']
+}
+
 if (!process.env.TRACKERS_FILE) {
   throw Error('TRACKERS_FILE is required')
 }
