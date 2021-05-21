36b42c3378bfeb9168c9fd19d5713b94afa6d25b
phillmac <phillmackintosh@gmail.com>
2021-05-21 07:43:32 +0000

    Use hkeysAsync

--- src/preload.js
+++ src/preload.js
@@ -48,8 +48,8 @@ const { redisClient, torrentsFromPreload } = require('./utils.js')
 
 async function preload () {
   const torrents = await torrentsFromPreload(process.env.PRELOAD_URL)
-  const existing = await redisClient.hkeys('torrents')
-  const missing = torrents.filter(t => !(existing && existing.includes(t._id)))
+  const existing = await redisClient.hkeysAsync('torrents')
+  const missing = torrents.filter(t => !( existing.includes(t._id)))
   console.info(`${missing.length} torrents to add`)
 
   for (const t of missing) {
