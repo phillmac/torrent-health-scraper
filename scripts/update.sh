a656c7f4746a5027af433174742eb3f461bd713b
phillmac <phillmackintosh@gmail.com>
2021-11-09 23:17:54 +0000

    Add update.sh

---
+++ scripts/update.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [[ -z "REDIS_HOST" ]]; then
+    export REDIS_HOST=redis
+fi
+
+if [[ -z "$REDIS_PORT" ]]; then
+    export REDIS_PORT=6379
+fi
+
+url=${1%/}
+collection_type=$2
+
+while read -r file_name
+do
+    echo "Adding ${file_name}"
+    node --unhandled-rejections=strict src/update.js --torrent-url "${url}/${file_name}" --type "${collection_type}"
+done < <(curl --silent -L "${url}" | grep -o "[a-zA-Z0-9./?=_%:-]*\.torrent" | sort -ru)
